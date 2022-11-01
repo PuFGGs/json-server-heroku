e8e18b01324a760912d2e1252f7ec439722cfe67
PuFGGs <dorukakin35@hotmail.com>
2022-11-01 01:10:19 +0000

    update

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const jsonServer = require('json-server')
 
-server.use(middlewares);
-server.use(router);
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+var db = router.db.chain()
 
-server.listen(port);
+server.use(middlewares)
+
+server.get('/users', (req, res) => {
+    var user = db.get('users').find({username: req.query.username, password: req.query.password}).value() || res.jsonp(null);
+    res.jsonp(user);
+    console.log(user);
+})
+
+server.get('/users/*', (req, res) => {
+    res.sendStatus(401)
+    console.log("rejected query");
+})
+
+
+
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
